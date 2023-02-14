1ae7bba904b68b7f2878ab3606d2e5b8a2ac34d2
dkarmm <dartikarmeev@mail.ru>
2023-02-14 21:39:49 +0500

    create example031.

---
+++ example031/Program.cs
@@ -0,0 +1,27 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+Console.Clear();
+
+void addNumbers (int [] array)
+{
+    int index = 0;
+    int size = array.Length;
+    while (index < size)
+    {
+        array[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void showNumbers (int [] array)
+{
+    int index = 0;
+    int size = array.Length;
+    while (index < size)
+    {
+        Console.Write(array[index] + " ");
+        index++;
+    }
+}
+int [] array = new int [8];
+addNumbers(array);
+showNumbers(array);
+Console.WriteLine();
